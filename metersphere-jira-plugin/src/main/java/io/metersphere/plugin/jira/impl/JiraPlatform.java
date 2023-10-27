6eb6ed9a8ef06938289a81bff7eb6ef242f36f5e
WangXu10 <xu.wang@fit2cloud.com>
2023-10-27 13:47:53 +0800

    feat(jira): 插件下拉options接口

--- metersphere-jira-plugin/src/main/java/io/metersphere/plugin/jira/impl/JiraPlatform.java
+++ metersphere-jira-plugin/src/main/java/io/metersphere/plugin/jira/impl/JiraPlatform.java
@@ -5,12 +5,18 @@ import io.metersphere.plugin.jira.client.JiraDefaultClient;
 import io.metersphere.plugin.jira.domain.JiraIntegrationConfig;
 import io.metersphere.plugin.jira.domain.JiraIssueProject;
 import io.metersphere.plugin.jira.domain.JiraProjectConfig;
+import io.metersphere.plugin.platform.dto.PlatformCustomFieldItemDTO;
 import io.metersphere.plugin.platform.dto.PlatformRequest;
+import io.metersphere.plugin.platform.dto.PluginOptionsRequest;
+import io.metersphere.plugin.platform.dto.SelectOption;
 import io.metersphere.plugin.platform.spi.AbstractPlatform;
 import io.metersphere.plugin.sdk.util.MSPluginException;
 import io.metersphere.plugin.sdk.util.PluginUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.pf4j.Extension;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @author jianxing
@@ -43,6 +49,35 @@ public class JiraPlatform extends AbstractPlatform {
         } catch (Exception e) {
             throw new MSPluginException(e.getMessage());
         }
+    }
+
+    @Override
+    public boolean isThirdPartTemplateSupport() {
+        return true;
+    }
+
+    @Override
+    public List<PlatformCustomFieldItemDTO> getThirdPartCustomField(String projectConfig) {
+        return null;
+    }
+
+    @Override
+    public List<SelectOption> getPluginOptions(PluginOptionsRequest request) {
+        return getFormOptions(this, request);
+    }
+
+
+    /**
+     * 由 getFormOptions 反射调用
+     *
+     * @return
+     */
+    public List<SelectOption> getBugType(PluginOptionsRequest request) {
+        JiraProjectConfig projectConfig = getProjectConfig(request.getProjectConfig());
+        return jiraClient.getIssueType(projectConfig.getJiraKey())
+                .stream()
+                .map(item -> new SelectOption(item.getName(), item.getId()))
+                .collect(Collectors.toList());
     }
 
 
