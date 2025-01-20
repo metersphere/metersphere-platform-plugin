cf51e79dbb9581478315aa0f1ebfd317fa219fe6
song-cc-rock <changchang.song@fit2cloud.com>
2025-01-20 18:29:07 +0800

    chore: modify zentao plugin version

--- metersphere-zentao-plugin/src/main/java/io/metersphere/platform/impl/ZentaoPlatformMetaInfo.java
+++ metersphere-zentao-plugin/src/main/java/io/metersphere/platform/impl/ZentaoPlatformMetaInfo.java
@@ -12,7 +12,7 @@ public class ZentaoPlatformMetaInfo extends AbstractPlatformMetaInfo {
 
     @Override
     public String getVersion() {
-        return "2.10.10";
+        return "2.10.11";
     }
 
     @Override
